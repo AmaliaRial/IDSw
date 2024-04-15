7c048aedfd0d9d990d91fec03e5a3b70a0987589
AmaliaRial <amalia.rialplaza@usp.ceu.es>
2024-04-15 20:09:11 +0200

    POJOS created

---
+++ IDSw/src/idsw/db/pojos/Virtual_Person.java
@@ -0,0 +1,103 @@
+package idsw.db.pojos;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import idsw.db.enums.*;
+
+public class Virtual_Person implements Serializable{
+		
+		/**
+		* 
+	 	*/
+		private static final long serialVersionUID = 6152860231636655190L;
+		private Integer idVirtual_Person;
+		private state state;
+		private Float disease_countdown;
+		private Float immunity_countdown;
+		private Virtual_Population virtual_population;
+		
+		
+		public Virtual_Person() {
+			super();
+		}
+
+
+		public Integer getIdVirtual_Person() {
+			return idVirtual_Person;
+		}
+
+
+		public void setIdVirtual_Person(Integer idVirtual_Person) {
+			this.idVirtual_Person = idVirtual_Person;
+		}
+
+
+		public state getState() {
+			return state;
+		}
+
+
+		public void setState(state state) {
+			this.state = state;
+		}
+
+
+		public Float getDisease_countdown() {
+			return disease_countdown;
+		}
+
+
+		public void setDisease_countdown(Float disease_countdown) {
+			this.disease_countdown = disease_countdown;
+		}
+
+
+		public Float getImmunity_countdown() {
+			return immunity_countdown;
+		}
+
+
+		public void setImmunity_countdown(Float immunity_countdown) {
+			this.immunity_countdown = immunity_countdown;
+		}
+
+
+		public Virtual_Population getVirtual_population() {
+			return virtual_population;
+		}
+
+
+		public void setVirtual_population(Virtual_Population virtual_population) {
+			this.virtual_population = virtual_population;
+		}
+
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(idVirtual_Person);
+		}
+
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Virtual_Person other = (Virtual_Person) obj;
+			return Objects.equals(idVirtual_Person, other.idVirtual_Person);
+		}
+
+
+		@Override
+		public String toString() {
+			return "Virtual_Person [idVirtual_Person=" + idVirtual_Person + ", state=" + state + ", disease_countdown="
+					+ disease_countdown + ", immunity_countdown=" + immunity_countdown + ", virtual_population="
+					+ virtual_population + "]";
+		}
+		
+		
+}
---
+++ IDSw/src/idsw/db/pojos/Virtual_Population.java
@@ -0,0 +1,90 @@
+package idsw.db.pojos;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Virtual_Population implements Serializable{
+	
+		/**
+		* 
+	 	*/
+		private static final long serialVersionUID = 5867685344047960434L;
+		private Integer initial_population; //this is not the primary key --> equals and hashcode????? Ask Rodrigo
+		private Float p_infected;
+		private Float p_healthy;
+		private Float p_immune;
+		private Integer immunity_period; //From disease
+		private Disease disease;
+		private List<Virtual_Person> virtual_people;
+		
+		public Virtual_Population() {
+			super();
+			this.virtual_people = new ArrayList<Virtual_Person>();
+		}
+
+		public Integer getInitial_population() {
+			return initial_population;
+		}
+
+		public void setInitial_population(Integer initial_population) {
+			this.initial_population = initial_population;
+		}
+
+		public Float getP_infected() {
+			return p_infected;
+		}
+
+		public void setP_infected(Float p_infected) {
+			this.p_infected = p_infected;
+		}
+
+		public Float getP_healthy() {
+			return p_healthy;
+		}
+
+		public void setP_healthy(Float p_healthy) {
+			this.p_healthy = p_healthy;
+		}
+
+		public Float getP_immune() {
+			return p_immune;
+		}
+
+		public void setP_immune(Float p_immune) {
+			this.p_immune = p_immune;
+		}
+
+		public Integer getImmunity_period() {
+			return immunity_period;
+		}
+
+		public void setImmunity_period(Integer immunity_period) {
+			this.immunity_period = immunity_period;
+		}
+
+		public Disease getDisease() {
+			return disease;
+		}
+
+		public void setDisease(Disease disease) {
+			this.disease = disease;
+		}
+
+		public List<Virtual_Person> getVirtual_people() {
+			return virtual_people;
+		}
+
+		public void setVirtual_people(List<Virtual_Person> virtual_people) {
+			this.virtual_people = virtual_people;
+		}
+
+		@Override
+		public String toString() {
+			return "Virtual_Population [initial_population=" + initial_population + ", p_infected=" + p_infected
+					+ ", p_healthy=" + p_healthy + ", p_immune=" + p_immune + ", immunity_period=" + immunity_period
+					+ ", disease=" + disease + ", virtual_people=" + virtual_people + "]";
+		}
+		
+		
+}

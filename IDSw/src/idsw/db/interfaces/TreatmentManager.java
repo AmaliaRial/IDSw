3a3a40eeefd55a4227bf0805bdb29513a000d71e
AmaliaRial <amalia.rialplaza@usp.ceu.es>
2024-04-18 14:06:46 +0200

    Created Treatment Interface

--- IDSw/src/idsw/db/interfaces/TreatmentManager.java
+++ IDSw/src/idsw/db/interfaces/TreatmentManager.java
@@ -1,5 +1,16 @@
 package idsw.db.interfaces;
 
+import java.util.List;
+
+import idsw.db.pojos.Treatment;
+
 public interface TreatmentManager {
+	
+	public List<Treatment> listSixRecentTreatment();
+	public List<Treatment> listMatchingTreatmentsByName(String search);
+	public Treatment getTreatment( int idTreatment);
+	public void deleteTreatment(Treatment treatment);
+	public void addTreatment(Treatment treatment);
+	public void modifyTreatment(Treatment treatment);
 
 }

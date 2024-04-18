a985925e23826e7c8ccf53ce71e916d59e13a903
AmaliaRial <amalia.rialplaza@usp.ceu.es>
2024-04-18 13:52:10 +0200

    Created diagnosis interface

--- IDSw/src/idsw/db/interfaces/DiagnosisManager.java
+++ IDSw/src/idsw/db/interfaces/DiagnosisManager.java
@@ -1,5 +1,16 @@
 package idsw.db.interfaces;
 
+import java.util.List;
+
+import idsw.db.pojos.Diagnosis;
+
 public interface DiagnosisManager {
+	
+	public List<Diagnosis> listSixRecentDiagnosis();
+	public List<Diagnosis> listAllDiagnosis();
+	public Diagnosis getDiagnosis( int idDiagnosis);
+	public void deleteDiagnosis(Diagnosis diagnosis);
+	public void addDiagnosis(Diagnosis diagnosis);
+	public void modifyDiagnosis(Diagnosis diagnosis);
 
 }

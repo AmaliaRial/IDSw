8a34bf5be0a15424c414435a2afe4302b5ef5af0
AmaliaRial <amalia.rialplaza@usp.ceu.es>
2024-04-18 13:57:25 +0200

    Created Medical record Interface

--- IDSw/src/idsw/db/interfaces/MedicalRecordManager.java
+++ IDSw/src/idsw/db/interfaces/MedicalRecordManager.java
@@ -1,5 +1,12 @@
 package idsw.db.interfaces;
 
+import idsw.db.pojos.Medical_Record;
+
 public interface MedicalRecordManager {
+	
+	public Medical_Record getMedical_Record ( int idMedical_record);
+	public void addMedicalReport(Medical_Record medicalReport);
+	public void modifyMedical_Record ( int idMedical_record);
+
 
 }

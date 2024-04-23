7f4b106f2aeab1500e17278ca181adba1cd79282
AmaliaRial <amalia.rialplaza@usp.ceu.es>
2024-04-23 18:01:07 +0200

    parametro actualizado

--- IDSw/src/idsw/db/jdbcInterfaces/DiseaseManager.java
+++ IDSw/src/idsw/db/jdbcInterfaces/DiseaseManager.java
@@ -8,7 +8,7 @@ import idsw.db.pojos.Symptom;
 public interface DiseaseManager {
 
 	public List<Disease> listSixRecentDiseases(); //in SQL: SELECT * FROM disease ORDER BY idDisease DESC LIMIT 6;
-	public List<Disease> listMatchingDiseaseByName(String search);
+	public List<Disease> listMatchingDiseaseByName(String name);
 	public List<Symptom> listMatchingDiseaseBySymptoms(List<Symptom> symptoms);
 	public Disease getDisease( int idDisease);
 	public void deleteDisease(Disease disease);

8ce284228f52349304c7dda6dba99aa713dddce0
AmaliaRial <amalia.rialplaza@usp.ceu.es>
2024-04-23 17:38:02 +0200

    interfaces actualizados

--- IDSw/src/idsw/db/jdbcInterfaces/VirtualPersonManager.java
+++ IDSw/src/idsw/db/jdbcInterfaces/VirtualPersonManager.java
@@ -1,5 +1,11 @@
 package idsw.db.jdbcInterfaces;
 
+import idsw.db.pojos.Virtual_Person;
+
 public interface VirtualPersonManager {
+	
+	public Virtual_Person getVirtualPerson(int idVirtual_person);
+	public void addVirtualPerson(Virtual_Person virtual_person);
+
 
 }
--- IDSw/src/idsw/db/jdbcInterfaces/VirtualPopulationManager.java
+++ IDSw/src/idsw/db/jdbcInterfaces/VirtualPopulationManager.java
@@ -1,5 +1,11 @@
 package idsw.db.jdbcInterfaces;
 
+import idsw.db.pojos.Virtual_Population;
+
 public interface VirtualPopulationManager {
+	
+	public void addVirtualPopulation(Virtual_Population virtualPopulation);
+	public Virtual_Population getVirtualPopulation(int idVirtual_Population);
+
 
 }

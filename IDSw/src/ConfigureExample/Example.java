8b54a3000d2159feb42465b0eb6e707066084055
AmaliaRial <amalia.rialplaza@usp.ceu.es>
2024-04-23 20:14:53 +0200

    TODO deleted

--- IDSw/src/ConfigureExample/Example.java
+++ IDSw/src/ConfigureExample/Example.java
@@ -3,7 +3,6 @@ package ConfigureExample;
 public class Example {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Bye World");
 		System.out.println("Rodrigo");

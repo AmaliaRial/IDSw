472b7dfec003c02ca47ea0d08762394264fb35b6
RodriFS0 <rodrigo.fernandezsanchez@usp.ceu.es>
2024-05-13 17:36:12 +0200

    correct errors, and continue with swing

--- IDSw/src/ids/db/graphicInterface/HomePanel.java
+++ IDSw/src/ids/db/graphicInterface/HomePanel.java
@@ -1,5 +1,20 @@
 package ids.db.graphicInterface;
 
-public class HomePanel {
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.*;
+
+public class HomePanel extends JPanel {
+	public HomePanel(){
+		setLayout(new BorderLayout());
+		JPanel northPanel = new JPanel(new GridBagLayout());
+		JPanel mediumPanel=new JPanel();
+		
+	}
+	
 
 }
--- IDSw/src/idsw/db/jdbcInterfaces/SimulationManager.java
+++ IDSw/src/idsw/db/jdbcInterfaces/SimulationManager.java
@@ -7,7 +7,7 @@ import idsw.db.pojos.Virtual_Population;
 
 public interface SimulationManager {
 	
-	public void addSimulation(Virtual_Population virtualPopulation);
+	public void addSimulation(Simulation simulation);
 	public Simulation selectSimulation(Integer simulation_id);
 	public List<Simulation> listMatchingSimulationByV_Population(Integer disease_id);
 	public Simulation createSimulation(Virtual_Population virtualPopulation);

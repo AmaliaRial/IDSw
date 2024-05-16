d0a6ddd6ae87b70940a8b61f7892019f9d471aff
Jorge Garcia <jorge.garcia13@usp.ceu.es>
2024-05-16 16:46:35 +0200

    New methode

--- IDSw/src/idsw/db/jdbcInterfaces/DiagnosisManager.java
+++ IDSw/src/idsw/db/jdbcInterfaces/DiagnosisManager.java
@@ -3,11 +3,15 @@ package idsw.db.jdbcInterfaces;
 import java.util.List;
 
 import idsw.db.pojos.Diagnosis;
+import idsw.db.pojos.Disease;
+import idsw.db.pojos.Patient;
+import idsw.db.pojos.Symptom;
 
 public interface DiagnosisManager {
 	
 	public List<Diagnosis> listSixRecentDiagnosis();
 	public List<Diagnosis> listAllDiagnosis();
+	public List<Diagnosis> listMatchinDiagnosesByPatient(Patient patient);
 	public Diagnosis getDiagnosis( int idDiagnosis);
 	public void deleteDiagnosis(int idDiagnosis);
 	public void addDiagnosis(Diagnosis diagnosis);

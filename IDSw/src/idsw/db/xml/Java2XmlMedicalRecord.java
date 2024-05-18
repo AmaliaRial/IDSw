1157a759f2392f0665ad28ddff6af91b83cf793e
Amalia Rial Plaza <160157186+AmaliaRial@users.noreply.github.com>
2024-05-18 23:56:12 +0200

    New Method to creatre an xml file from gui added

--- IDSw/src/idsw/db/xml/Java2XmlMedicalRecord.java
+++ IDSw/src/idsw/db/xml/Java2XmlMedicalRecord.java
@@ -13,7 +13,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
 
 import idsw.db.jdbc.ConnectionManager;
 import idsw.db.jdbcInterfaces.MedicalRecordManager;
@@ -28,6 +30,103 @@ public class Java2XmlMedicalRecord {
 	private static Connection c ;
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     private static ConnectionManager conMan = new ConnectionManager();
+    
+    public static void createXML(int record_id) {
+        // Get a connection to the database
+        try {
+			c = DriverManager.getConnection("jdbc:sqlite:./db/idsw.db"); // replace with your database connection string
+
+			// Create the JAXBContext
+			JAXBContext jaxbContext = JAXBContext.newInstance(Medical_Record.class);
+			// Get the marshaller
+			Marshaller marshaller = jaxbContext.createMarshaller();
+
+			// Pretty formatting
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			
+			String selectSQL = "SELECT * FROM medical_records WHERE idMedical_Record = ?";
+			PreparedStatement ps = c.prepareStatement(selectSQL);
+			ps.setInt(1, record_id);
+			ResultSet rs = ps.executeQuery();
+			Medical_Record record = null;
+			if (rs.next()) {
+
+				Integer id = rs.getInt("IDMedical_Record");
+				Integer patient_id = rs.getInt("patient");
+				Patient patient = conMan.getPatientMan().getPatient(patient_id);
+				Date dob = patient.getDob();
+				patient.setDob(dob);
+				record = new Medical_Record(id, patient);
+			    
+				// Fetch the diagnoses for the current medical record
+			    String selectDiagnosesSQL = "SELECT * FROM diagnoses WHERE medicalRecord_id = ?";
+			    PreparedStatement psDiagnoses = c.prepareStatement(selectDiagnosesSQL);
+			    psDiagnoses.setInt(1, id);
+			    ResultSet rsDiagnoses = psDiagnoses.executeQuery();
+			    List<Diagnosis> diagnoses = new ArrayList<>();
+			    while (rsDiagnoses.next()) {
+					Integer idDiag = rsDiagnoses.getInt("IDdiagnosis");
+					String name = rsDiagnoses.getString("nameDiagnosis");
+					Date date = rsDiagnoses.getDate("date");
+					String comments = rsDiagnoses.getString("comment_section");
+					Integer idDisease = rsDiagnoses.getInt("disease_id");
+					Disease disease = conMan.getDiseaseMan().getDisease(idDisease);
+					Integer idMedicalRecord =rsDiagnoses.getInt("medicalrecord_id"); 
+					Medical_Record medicalRecord = conMan.getMedicalRecordMan().getMedical_Record(idMedicalRecord);
+					Diagnosis diagnosis = new Diagnosis(idDiag, name, date, comments,disease);
+					
+					// Fetch the treatments for the current diagnosis
+			        String selectTreatmentsSQL = "SELECT treatments.* FROM treatments "
+			                                    + "JOIN diagnosis_has_treatments ON IDtreatment = treatment_id WHERE diagnosis_id = ?";
+			        PreparedStatement psTreatments = c.prepareStatement(selectTreatmentsSQL);
+			        psTreatments.setInt(1, idDiag);
+			        ResultSet rsTreatments = psTreatments.executeQuery();
+			        List<Treatment> treatments = new ArrayList<>();
+			        while (rsTreatments.next()) {
+			        	String treatmentName = rsTreatments.getString("nameTreatment");
+						String comment_section = rsTreatments.getString("comment_section");
+						Treatment treatment = new Treatment(treatmentName, comment_section);
+						treatments.add(treatment);
+			        }
+			        rsTreatments.close();
+			        psTreatments.close();
+
+			        // Set the treatments to the diagnosis
+			        diagnosis.setTreatments(treatments);
+					
+					diagnoses.add(diagnosis);			
+			    }
+			    rsDiagnoses.close();
+			    psDiagnoses.close();
+
+			    // Set the diagnoses to the medical record
+			    record.setDiagnoses(diagnoses);
+			}
+			rs.close();
+			ps.close();
+
+			try {
+			    // Use the Marshaller to marshal the Java object to a file
+			    File directory = new File("./xmls/");
+			    if (! directory.exists()){
+			        boolean dirCreated = directory.mkdir();
+			        System.out.println("Directory created: " + dirCreated);
+			    }
+			    File file = new File("./xmls/MedicalRecord.xml");
+			    marshaller.marshal(record, file);
+			    // Printout
+			    marshaller.marshal(record, System.out);
+			} catch (Exception e) {
+			    e.printStackTrace();
+			}
+		} catch (PropertyException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+    }
     
     private static void printMedicalRecords(Connection c) throws SQLException {
         String selectSQL = "SELECT * FROM medical_records";
@@ -89,7 +188,7 @@ public class Java2XmlMedicalRecord {
     }
 
     public static void main(String[] args) throws Exception {
-        // Get a connection to the database
+        /*// Get a connection to the database
         c = DriverManager.getConnection("jdbc:sqlite:./db/idsw.db"); // replace with your database connection string
 
         // Create the JAXBContext
@@ -164,7 +263,7 @@ public class Java2XmlMedicalRecord {
         }
         rs.close();
         ps.close();
-
+        
         try {
             // Use the Marshaller to marshal the Java object to a file
             File directory = new File("./xmls/");
@@ -179,5 +278,7 @@ public class Java2XmlMedicalRecord {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        */
+    	//createXML(1);
     }
 }

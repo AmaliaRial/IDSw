8424afab0aa0ec1f1663a52fc98bf3ca85ffb61b
loreeenacano <lorena.canodiazmaroto@usp.ceu.es>
2024-05-20 17:26:33 +0200

    new panels created

--- IDSw/src/idsw/db/graphicInterface/ViewDiagnosisPanelForDoctor.java
+++ IDSw/src/idsw/db/graphicInterface/ViewDiagnosisPanelForDoctor.java
@@ -1,5 +1,29 @@
 package idsw.db.graphicInterface;
 
-public class ViewDiagnosisPanelForDoctor {
+import javax.swing.*;
+import idsw.db.graphicInterface.components.*;
+import java.awt.*;
+import idsw.db.pojos.*;
 
+public class ViewDiagnosisPanelForDoctor extends ViewDiagnosisPanel{
+public RoundedButton continueButton;
+	
+	public ViewDiagnosisPanelForDoctor(Integer id_diagnosis) {
+		super(id_diagnosis);
+		this.continueButton = new RoundedButton("Continue", Color.decode("#09A8E4"));	
+		this.continueButton.setPreferredSize(new Dimension(110, 30));
+		this.backCalcelPanel.add(this.continueButton);
+    }
+
+	public static void main(String[] args) {
+        // Crear y mostrar la ventana de prueba
+        JFrame frame = new JFrame("Ejemplo con Swing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new ViewDiagnosisPanelForDoctor(1));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
+
+
--- IDSw/src/idsw/db/graphicInterface/ViewDiseasePanelForDoctor.java
+++ IDSw/src/idsw/db/graphicInterface/ViewDiseasePanelForDoctor.java
@@ -1,5 +1,28 @@
 package idsw.db.graphicInterface;
 
-public class ViewDiseasePanelForDoctor {
+import javax.swing.*;
+import idsw.db.graphicInterface.components.*;
+import java.awt.*;
+import idsw.db.pojos.*;
 
+
+public class ViewDiseasePanelForDoctor extends ViewDiseasePanel {
+	public RoundedButton continueButton;
+	
+	public ViewDiseasePanelForDoctor(Integer id_disease) {
+		super(id_disease);
+		this.continueButton = new RoundedButton("Continue", Color.decode("#09A8E4"));	
+		this.continueButton.setPreferredSize(new Dimension(110, 30));
+		this.backCalcelPanel.add(this.continueButton);
+    }
+
+	public static void main(String[] args) {
+        // Crear y mostrar la ventana de prueba
+        JFrame frame = new JFrame("Ejemplo con Swing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new ViewDiseasePanelForDoctor(1));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
